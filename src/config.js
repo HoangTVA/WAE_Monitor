c23304b18388a51f70603c0e2c280bdf4c3e2ad9
Hoang Thuy Vo Anh <hoangtvasr150780@gmail.com>
2022-02-23 23:12:19 +0700

    First commit

---
+++ src/config.js
@@ -0,0 +1,24 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+export const cognitoConfig = {
+  userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOL_ID,
+  clientId: process.env.REACT_APP_AWS_COGNITO_CLIENT_ID
+};
+
+export const auth0Config = {
+  clientId: '2CyhsVcspquD5rh0J3Zikt82MZtjeshO',
+  domain: 'dev-a4ey75li.us.auth0.com'
+};
+
+export const mapConfig = process.env.REACT_APP_MAP_MAPBOX;
+
+export const googleAnalyticsConfig = process.env.REACT_APP_GA_MEASUREMENT_ID;
